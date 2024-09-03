40d6ed198edc5fd08bdcf46371a2c62eec2eb912
Nando Rocha <nando.souza.rocha@gmail.com>
2024-09-03 16:19:06 +0100

    Added a thank_you.html file, and also made changes in views.py and urls.py

--- reviews/views.py
+++ reviews/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 
 def review(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        print(username)
+        return HttpResponseRedirect('/thank-you')
+    
     return render(request, "reviews/review.html")
+
+
+def thank_you(request):
+    return render(request, "reviews/thank_you.html")
