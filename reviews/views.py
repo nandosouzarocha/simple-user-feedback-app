8014bb5efd8b1cfe4b4b86f2728b7e301936486a
Nando Rocha <nando.souza.rocha@gmail.com>
2024-09-26 11:29:41 +0100

    SingleReviewView class was changed. This class is inheriting off DetailView now. Its correspondent URL was modified as well.

--- reviews/views.py
+++ reviews/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.views import View
 from django.views.generic.base import TemplateView
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .forms import ReviewForm
 from .models import Review
 
@@ -41,12 +41,6 @@ class ReviewsListView(ListView):
     model = Review
     context_object_name = 'reviews'
     
-class SingleReviewView(TemplateView):
+class SingleReviewView(DetailView):
     template_name = 'reviews/single_review.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        review_id = kwargs['id']
-        selected_review = Review.objects.get(pk=review_id)
-        context['review'] = selected_review
-        return context
+    model = Review
